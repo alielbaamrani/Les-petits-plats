74fc43c220784daa8ae60a031a23885a3612dd5c
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-04-20 15:55:42 +0200

    p7v0.5.0

--- src/scripts/components/dropdown.js
+++ src/scripts/components/dropdown.js
@@ -1,5 +1,44 @@
-const button = document.querySelector('.accordion-button')
+const inputIngredient = document.querySelector('.accordion-button-ingredients')
+const accordionIngredient = document.querySelector('#ingredients')
 
-button.addEventListener('click', () => {
-  console.log('test')
+const inputAppareils = document.querySelector('.accordion-button-appareils')
+const accordionAppareils = document.querySelector('#appareils')
+
+const inputUstensibles = document.querySelector('.accordion-button-ustensibles')
+const accordionUstensibles = document.querySelector('#ustensibles')
+
+inputIngredient.addEventListener('click', () => {
+  if (inputIngredient.type === 'button') {
+    accordionIngredient.style.maxWidth = '45vw'
+    inputIngredient.type = 'text'
+    inputIngredient.value = ''
+  } else {
+    inputIngredient.type = 'button'
+    inputIngredient.value = 'ingredients'
+    accordionIngredient.style.maxWidth = ''
+  }
+})
+
+inputAppareils.addEventListener('click', () => {
+  if (inputAppareils.type === 'button') {
+    accordionAppareils.style.maxWidth = '45vw'
+    inputAppareils.type = 'text'
+    inputAppareils.value = ''
+  } else {
+    inputAppareils.type = 'button'
+    inputAppareils.value = 'appareils'
+    accordionAppareils.style.maxWidth = ''
+  }
+})
+
+inputUstensibles.addEventListener('click', () => {
+  if (inputUstensibles.type === 'button') {
+    accordionUstensibles.style.maxWidth = '45vw'
+    inputUstensibles.type = 'text'
+    inputUstensibles.value = ''
+  } else {
+    inputUstensibles.type = 'button'
+    inputUstensibles.value = 'ustensibles'
+    accordionUstensibles.style.maxWidth = ''
+  }
 })
