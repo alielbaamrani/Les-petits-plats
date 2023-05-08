959700f98c823392a74ad5c122836db9f6a46894
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-05-08 15:10:16 +0200

    Correction Algorithme #1

--- src/scripts/components/search.js
+++ src/scripts/components/search.js
@@ -1,6 +1,15 @@
+/**
+ * returns the first element in the provided array that satisfies the provided testing function (isIncluded function).
+ * If no values satisfy the testing function, undefined is returned.
+ * @param {Array} array - Array of object (recipes)
+ * @param {String} property - Object property by example: 'description' or 'name'
+ * @param {String} value - String to search
+ * @returns Object || undefined
+ */
+const isFound = (array, property, value) => array.find(item => isLowerCaseIncluded(item[property], value))
+
 export const mainSearch = (recipes, value) => recipes.filter(el => el.name.toLowerCase().includes(value) || el.description.toLowerCase().includes(value) || isFound(el.ingredients, 'ingredient', value))
 
-const isFound = (array, property, value) => array.find(item => isLowerCaseIncluded(item[property], value))
 export const isLowerCaseIncluded = (value1, value2) => value1.toLowerCase().includes(value2.toLowerCase())
 
 const isRecipeIncludesEveryTagIngredient = (recipe, tags) => tags.ingredients.every(ingredient => recipe.ingredients.map(elem => elem.ingredient).includes(ingredient))
@@ -15,5 +24,5 @@ const isRecipeIncludesEveryTagUstensil = (recipe, tags) => tags.ustensils.every(
  */
 export const filterByTags = (recipes, tags) => recipes.filter(recipe =>
   isRecipeIncludesEveryTagIngredient(recipe, tags) &&
-    isRecipeIncludesEveryTagAppliance(recipe, tags) &&
-    isRecipeIncludesEveryTagUstensil(recipe, tags))
+  isRecipeIncludesEveryTagAppliance(recipe, tags) &&
+  isRecipeIncludesEveryTagUstensil(recipe, tags))
